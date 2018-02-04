2c8afb910c029ff0cd5a105ed0a6318cac053a81
Ahmad El-Saka <elsa2a@live.com>
2018-02-04 23:03:44 +0200

    Additions to Morning Azkar
    Addition of the following:
    - Arrays of the morning azkars.
    - Setting the tableview of the morning azkar.
    - Adjusting the Zekr texts and counters.

---
+++ Azkar/AzkarTexts.swift
@@ -0,0 +1,11 @@
+//
+//  AzkarTexts.swift
+//  Azkar
+//
+//  Created by Ahmad on 2/4/18.
+//  Copyright © 2018 Ahmad. All rights reserved.
+//
+
+import Foundation
+
+
---
+++ Azkar/Cells/MorningTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MorningTableViewCell.swift
+//  Azkar
+//
+//  Created by Ahmad on 2/4/18.
+//  Copyright © 2018 Ahmad. All rights reserved.
+//
+
+import UIKit
+
+class MorningTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var ZekrLabelView: UILabel!
+    
+    @IBOutlet weak var ZekrCounter: UIButton!
+    
+    
+   
+    
+    
+    func fillData(txt:String , count:String)  {
+   
+      
+        ZekrLabelView.text = txt
+        ZekrCounter.setTitle("تكرار: " + count, for: .normal)
+        //ZekrCounter.titleLabel?.text = count
+    }
+}
+
